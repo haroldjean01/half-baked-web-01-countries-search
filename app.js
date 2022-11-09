b49634ea1aa48eb71340e6c4b6cecf29f5256ba6
Harold Jean <haroldjean38@yahoo.com>
2022-11-09 16:25:47 -0500

    added events

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* Imports */
 // Slice A: import getCountries from fetch-utils.js
+import { getCountries } from './fetch-utils.js';
 // Slice B: import getContinents from fetch-utils.js
+import { getContinents } from './fetch-utils.js';
 
 import { renderContinentOption, renderCountry } from './render-utils.js';
 
@@ -18,35 +20,9 @@ window.addEventListener('load', async () => {
     // call findCountries function with no arguments to fetch all countries (Slice A);
     findCountries();
     // Slice B: call asynchronous getContinents fetch function and set to response variable
+    const response = await getContinents();
     // Slice B: set the continents state to the response.data
+    continents = response.data;
     // Slice B: call displayContinentOptions function;
+    displayContinentOptions();
 });
-
-async function findCountries(continent) {
-    // Slice A: call the asynchronous fetch function to get the countries
-    // Slice C: add continent argument to getCountries function call
-    // console log the response object to see all of the nested information returned
-    // Slice A: set the countries state to the response.data
-    // Slice A: call displayCountries function;
-}
-
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(searchForm);
-    // Slice C: Call findCountries with continent from formData
-});
-
-/* Display Functions */
-function displayCountries() {
-    //Slice A: reset the countries List
-
-    for (const country of countries) {
-        // Slice A: Call imported render countries function and append to list
-    }
-}
-
-function displayContinentOptions() {
-    for (const continent of continents) {
-        // Slice B: Call continent render function and append to continent selector
-    }
-}
